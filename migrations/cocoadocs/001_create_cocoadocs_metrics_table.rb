04e9fbb5d25761015043062762d9e1661d63b30c
Orta Therox <orta.therox@gmail.com>
2014-11-28 08:05:12 +0000

    cocoadocs support

---
+++ migrations/cocoadocs/001_create_cocoadocs_metrics_table.rb
@@ -0,0 +1,21 @@
+Sequel.migration do
+  change do
+    create_table(:cocoadocs_pod_metrics) do
+      primary_key :id
+      foreign_key :pod_id, :pods, :null => false, :key => [:id]
+
+      Integer :download_size, :null => true
+      Integer :total_files, :null => true
+
+      Integer :doc_percent, :null => true
+      Integer :readme_complexity, :null => true
+      DateTime :initial_commit_date, :null => true
+      String :rendered_readme_url, :null => true
+
+      Integer :not_found, :default => 0
+
+      DateTime :created_at
+      DateTime :updated_at
+    end
+  end
+end
